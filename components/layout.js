b513d81d6f55a31897fb2a28a7698406c9c60c8c
maniabang <madmanno@naver.com>
2023-08-22 23:56:10 +0900

    footer disable

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div className="bg-primary">
       <Header />
       <div>{children}</div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }

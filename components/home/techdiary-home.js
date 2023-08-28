ac5be836920fd0d84eb2e6b6b7d0ab0615b261ad
maniabang <madmanno@naver.com>
2023-08-28 22:34:09 +0900

    css

--- components/home/techdiary-home.js
+++ components/home/techdiary-home.js
@@ -2,7 +2,7 @@ import TechDiaryItem from "../techdiary/techdiary-item";
 
 export default function TechDiaryHome({ techdiary }) {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+    <section className="mt-10 flex flex-col items-center justify-center text-gray-600 body-font">
       <h1 className="text-4xl font-bold sm:text-6xl">
         총 프로젝트 :
         <span className="pl-4 text-blue-500">{techdiary?.results.length}</span>
--- pages/index.js
+++ pages/index.js
@@ -25,12 +25,10 @@ export default function Home({ techdiary }) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <Hero />
-          <Animation />
-        </div>
-      </section>
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <Hero />
+        <Animation />
+      </div>
 
       <section id="projects">
         <Projects />

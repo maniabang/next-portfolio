2ced069f622c83bf7b1b179c1b0cc28e77b153f4
Adam KwangHoon Lee <madmanno@naver.com>
2024-12-03 13:11:34 +0900

    Change Profile Photo

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ techdiary }) {
         />
         <meta
           property="og:image"
-          content="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ffecdaa1d-185f-4658-9cf2-ab0e49f01018%2FIMG_4267.jpg?table=block&id=fd6e98f6-b751-436d-aa62-88fdb427dd02&spaceId=ba40811f-21c0-4249-a512-9e75d1c965ff&width=2000&userId=c5d3630a-a8cd-46b1-9d83-bbffaf3d0263&cache=v2"
+          content="IMG_8687_Original.jpg"
         />
         <meta
           name="viewport"
